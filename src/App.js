fccd4ccf5b235e391c306f6163d74f4ca496c962
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-12-27 22:30:31 +0900

    Create Form Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form'
 
 class App extends Component {
   render() {
     return (
-      <TodoListTemplate>
+      <TodoListTemplate form={<Form />}>
         템플릿 완성
       </TodoListTemplate>
-    )
+    );
   }
 }
 
 export default App;
+
+{/*
+React Component구현 방법 : 
+  컴포턴트 DOM 태그 작성, CSS스타일 작성 -> 상태관리 및 props로 필요한 값 전달
+*/}
