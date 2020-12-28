380d26130a26f57ea8db7e1daf0f486f872981bf
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-12-28 14:54:09 +0900

    Define initial state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
 
 class App extends Component {
+
+  id = 3
+
+  state = {
+    input: '',
+    todos: [
+      { id: 0, text=' 리액트 소개', checked: false },
+      { id: 1, text=' 리액트 소개', checked: true },
+      { id: 2, text=' 리액트 소개', checked: false },
+    ]
+  }
   render() {
     return (
       <TodoListTemplate form={<Form />}>
@@ -15,7 +26,11 @@ class App extends Component {
 
 export default App;
 
-{/*React Component구현 방법 : 
-  컴포턴트 DOM 태그 작성, CSS스타일 작성 -> 상태관리 및 props로 필요한 값 전달*/}
-{/*‘리스트’ 를 렌더링하게 될 때(특히 보여주는 리스트가 동적인 경우) : 
-  함수형이 아닌 클래스형 컴포넌트로 작성한다. 클래스형 컴포넌트로 작성해야 나중에 컴포넌트 성능 최적화를 할 수 있기 때문이다.*/}
+{/* React Component구현 방법 : 
+  컴포턴트 DOM 태그 작성, CSS스타일 작성 -> 상태관리 및 props로 필요한 값 전달 */}
+{/* ‘리스트’ 를 렌더링하게 될 때(특히 보여주는 리스트가 동적인 경우) : 
+  함수형이 아닌 클래스형 컴포넌트로 작성한다. 클래스형 컴포넌트로 작성해야 나중에 컴포넌트 성능 최적화를 할 수 있기 때문이다. */}
+{/* 상태관리시 : 
+  컴포넌트끼리 데이터를 직접(ref를 사용해서) 전달하는것이 아니고, 
+  반드시 부모를 통하여 
+ */}
