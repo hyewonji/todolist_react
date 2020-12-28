cd35205187816bdefbd824ead760ad9f5157b7b3
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-12-28 16:57:11 +0900

    Implement Form submit

--- src/App.js
+++ src/App.js
@@ -10,14 +10,53 @@ class App extends Component {
   state = {
     input: '',
     todos: [
-      { id: 0, text=' 리액트 소개', checked: false },
-      { id: 1, text=' 리액트 소개', checked: true },
-      { id: 2, text=' 리액트 소개', checked: false },
+      { id: 0, text: ' 리액트 소개', checked: false },
+      { id: 1, text: ' 리액트 소개', checked: true },
+      { id: 2, text: ' 리액트 소개', checked: false },
     ]
   }
+
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    });
+  }
+
+  handleCreate = () => {
+    const { input, todos } = this.state;
+    this.setState({
+      input: '',
+      todos: todos.concat({ // push 대신 concat을 사용하는 이유 : push랄 사용하면 추가된 배열과 이전의 배열을 비교할 수 없다. == 최적화 할 수 없다.
+        id: this.id++,
+        text: input,
+        checked: false
+      })
+    });
+  }
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.handleCreate();
+    }
+  }
+
   render() {
+    const { input } = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress
+    } = this; // this.handleChange, this.handleCreate, this.handleKeyPress 이런식의 작업을 생략할 수 있다.
+
     return (
-      <TodoListTemplate form={<Form />}>
+      <TodoListTemplate form={(
+        <Form
+          value={input}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+          onCreate={handleCreate}
+        />
+      )}>
         <TodoItemList />
       </TodoListTemplate>
     );
@@ -26,11 +65,10 @@ class App extends Component {
 
 export default App;
 
-{/* React Component구현 방법 : 
-  컴포턴트 DOM 태그 작성, CSS스타일 작성 -> 상태관리 및 props로 필요한 값 전달 */}
-{/* ‘리스트’ 를 렌더링하게 될 때(특히 보여주는 리스트가 동적인 경우) : 
-  함수형이 아닌 클래스형 컴포넌트로 작성한다. 클래스형 컴포넌트로 작성해야 나중에 컴포넌트 성능 최적화를 할 수 있기 때문이다. */}
-{/* 상태관리시 : 
-  컴포넌트끼리 데이터를 직접(ref를 사용해서) 전달하는것이 아니고, 
-  반드시 부모를 통하여 
- */}
+/* React Component구현 방법 :
+  컴포턴트 DOM 태그 작성, CSS스타일 작성 -> 상태관리 및 props로 필요한 값 전달 */
+/* ‘리스트’ 를 렌더링하게 될 때(특히 보여주는 리스트가 동적인 경우) :
+  함수형이 아닌 클래스형 컴포넌트로 작성한다. 클래스형 컴포넌트로 작성해야 나중에 컴포넌트 성능 최적화를 할 수 있기 때문이다. */
+/* 상태관리시 :
+  컴포넌트끼리 데이터를 직접(ref를 사용해서) 전달하는것이 아니고,
+  반드시 부모를 통하여 전달한다.*/
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Form.css';
 
+/* Form 기능 : 
+    1. 텍스트 내용 바뀌면 state 업데이트
+    2. 버튼이 클릭되면 새로운 todo 생성 후 todos 업데이트
+    3. 인풋에서 Enter누르면 버튼 클릭한것과 동일한 작업 진행 */
 const Form = ({ value, onChange, onCreate, onKeyPress }) => {
     return (
         <div className="form">
-            <input value={value} onChange={onChange} onKeyPress={onKeyPress} />
+            <input value={value} onChange={onChange} onKeyPress={onKeyPress} /> {/*onKeyPress : 특정키에 대한 기능 설정*/}
             <div className="create-button" onClick={onCreate}>
                 추가
             </div>
