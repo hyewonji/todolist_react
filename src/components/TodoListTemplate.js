ad341044bedfbed0d8dcaa0c28b5d078b815c133
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-12-28 11:00:42 +0900

    from -> form 오류수정

--- src/components/TodoListTemplate.js
+++ src/components/TodoListTemplate.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './TodoListTemplate.css';
 
-const TodoListTemplate = ({ from, children }) => {
+const TodoListTemplate = ({ form, children }) => {
     return (
         <main className="todo-list-template">
             <div className="title">
                 오늘 할 일
             </div>
             <section className="form-wrapper">
-                {from}
+                {form}
             </section>
             <section className="todos-wrapper">
                 {children}
