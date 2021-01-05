40f771989d5f45f35bd5b4aebabec905bc2b91f3
hyewonji <60416187+hyewonji@users.noreply.github.com>
2021-01-06 00:03:21 +0900

    Show Remainder Number and Date

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -25,14 +25,23 @@ const TodoHeadBlock = styled.div`
 
 function TodoHead() {
     const todos = useTodoState();
-    console.log(todos);
+    const undoneTasks = todos.filter(todo => !todo.done);
+
+    const today = new Date();
+    const dateString = today.toLocaleDateString('ko-KR', {
+        year: 'numeric',
+        nonth: 'long',
+        day: 'numeric'
+    });
+    const dayName = today.toLocaleDateString('ko-KR', { weekday: 'long' });
+
     return (
         <TodoHeadBlock>
-            <h1>2021년 01월 03일</h1>
-            <div className="day">일요일</div>
-            <div className="tasks-left">할 일 2개 남음</div>
+            <h1>{ dateString }</h1>
+            <div className="day">{ dayName }</div>
+            <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
         </TodoHeadBlock>
-    )
+    );
 }
 
 export default TodoHead;
